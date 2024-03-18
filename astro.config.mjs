0377d17fe3165ed940228093051886be09d599f9
biati-digital <manuel@biati.digital>
2024-03-18 15:40:10 -0600

    chore: updated url

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "",
+  site: "https://glightbox.biati.digital",
   integrations: [
     mdx({
       syntaxHighlight: "shiki",
